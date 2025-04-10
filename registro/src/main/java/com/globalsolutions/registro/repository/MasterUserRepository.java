e5c6ddef2a5e24a0dbf022fe430e3004a6fdaf3b
Mariogs-99 <escobar.mario@globalsolutionslt.com>
2025-04-10 09:40:10 -0600

    Se elimino registro

--- registro/src/main/java/com/globalsolutions/registro/repository/MasterUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.globalsolutions.registro.repository;
-
-import com.globalsolutions.registro.model.MasterUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MasterUserRepository extends JpaRepository<MasterUser, Long> {
-    boolean existsByUsuario(String usuario);
-}
--- registro/src/main/java/com/globalsolutions/registro/service/impl/RegistroService.java
+++
@@ -1,7 +0,0 @@
-package com.globalsolutions.registro.service.impl;
-
-import com.globalsolutions.registro.model.RegistroClienteRequest;
-
-public interface RegistroService {
-    void procesarRegistro(RegistroClienteRequest request) throws Exception;
-}
